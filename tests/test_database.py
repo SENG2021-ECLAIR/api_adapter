7f89c94f7ae3ed78601ad7feac17b86a1c19dd3a
Peter Louka <p.louka@student.unsw.edu.au>
2022-03-26 01:46:03 +1100

    feature: testing login and logout

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,10 +1,6 @@
 import pytest
 
-from src.database import (  # , login_user, logout_user
-    connect_to_db,
-    create_user,
-    get_user,
-)
+from src.database import connect_to_db, get_user, login_user, logout_user, register_user
 
 test_user_data = {
     "email": "test@email.com",
@@ -16,7 +12,10 @@ test_user_data = {
 
 def cleanup(db, email):
     users = db["users"]
+    logged_in = db["logged_in"]
+
     users.delete_one({"email": email})
+    logged_in.delete_one({"email": email})
 
 
 @pytest.fixture
@@ -25,10 +24,7 @@ def db():
 
 
 def test_get_user(db):
-    """
-    assuming user test@email.com is registered
-    """
-    create_user(test_user_data)
+    register_user(test_user_data)
     user = get_user(test_user_data["email"])
 
     assert user["email"] == test_user_data["email"]
@@ -37,29 +33,137 @@ def test_get_user(db):
     cleanup(db, test_user_data["email"])
 
 
-def test_get_user_not_registered():
-    """
-    assuming user test@email.com isn't registered
-    """
+def test_get_user_not_registered(db):
     test_email = "notregistered@email.com"
+    cleanup(db, test_email)
     user = get_user(test_email)
+
     assert user is None
 
 
-def test_create_user(db):
+def test_register_user(db):
     users = db["users"]
     cleanup(db, test_user_data["email"])
-
     query = {"email": test_user_data["email"]}
 
     assert users.find_one(query) is None
 
-    response = create_user(test_user_data)
+    response = register_user(test_user_data)
 
-    assert users.find_one(query)
+    assert users.find_one(query) is not None
+    assert response == f"User {test_user_data['email']} registered"
 
-    response = create_user(test_user_data)
+    response = register_user(test_user_data)
 
     assert response == "user already registered"
 
     cleanup(db, test_user_data["email"])
+
+
+def test_login_user(db):
+    users = db["users"]
+    logged_in = db["logged_in"]
+    query = {"email": test_user_data["email"]}
+
+    # Register but not logged in
+    cleanup(db, test_user_data["email"])
+    _ = register_user(test_user_data)
+
+    assert users.find_one(query) is not None
+    assert logged_in.find_one(query) is None
+
+    token = login_user(test_user_data["email"], test_user_data["password"])
+    logged_in_user = logged_in.find_one(query)
+
+    assert token is not None
+    assert logged_in_user["token"] == token
+    assert logged_in_user["email"] == test_user_data["email"]
+
+
+def test_login_user_not_registered(db):
+    users = db["users"]
+    logged_in = db["logged_in"]
+    query = {"email": test_user_data["email"]}
+    cleanup(db, test_user_data["email"])
+    assert users.find_one(query) is None
+    token = login_user(test_user_data["email"], test_user_data["password"])
+
+    assert token is None
+    assert logged_in.find_one(query) is None
+
+
+def test_login_user_already_logged_in(db):
+    users = db["users"]
+    logged_in = db["logged_in"]
+    query = {"email": test_user_data["email"]}
+
+    # Register but not logged in
+    cleanup(db, test_user_data["email"])
+    _ = register_user(test_user_data)
+
+    assert users.find_one(query) is not None
+    assert logged_in.find_one(query) is None
+
+    token0 = login_user(test_user_data["email"], test_user_data["password"])
+    logged_in_user = logged_in.find_one(query)
+
+    assert token0 is not None
+    assert logged_in_user["token"] == token0
+    assert logged_in_user["email"] == test_user_data["email"]
+
+    token1 = login_user(test_user_data["email"], test_user_data["password"])
+    logged_in_user = logged_in.find_one(query)
+
+    assert token1 is None
+    assert logged_in_user["token"] != token1
+    assert logged_in_user["token"] == token0
+    assert logged_in_user["email"] == test_user_data["email"]
+
+
+def test_logout_user(db):
+    users = db["users"]
+    logged_in = db["logged_in"]
+    cleanup(db, test_user_data["email"])
+    query = {"email": test_user_data["email"]}
+    _ = register_user(test_user_data)
+
+    assert users.find_one(query) is not None
+    assert logged_in.find_one(query) is None
+
+    token0 = login_user(test_user_data["email"], test_user_data["password"])
+    logged_in_user = logged_in.find_one(query)
+
+    assert token0 is not None
+    assert logged_in_user["token"] == token0
+    assert logged_in_user["email"] == test_user_data["email"]
+
+    logout_user(token0)
+
+    logged_in_user = logged_in.find_one(query)
+
+    assert logged_in_user is None
+
+
+def test_logout_user_failed(db):
+    users = db["users"]
+    logged_in = db["logged_in"]
+    cleanup(db, test_user_data["email"])
+    query = {"email": test_user_data["email"]}
+    _ = register_user(test_user_data)
+
+    assert users.find_one(query) is not None
+    assert logged_in.find_one(query) is None
+
+    token0 = login_user(test_user_data["email"], test_user_data["password"])
+    logged_in_user = logged_in.find_one(query)
+
+    assert token0 is not None
+    assert logged_in_user["token"] == token0
+    assert logged_in_user["email"] == test_user_data["email"]
+
+    logout_user("not a real token")
+
+    logged_in_user = logged_in.find_one(query)
+
+    assert logged_in_user["token"] == token0
+    assert logged_in_user["email"] == test_user_data["email"]
