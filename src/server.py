7f3cd9d305e32cd27ed1f2a2fd1778b56639949b
Peter Louka <p.louka@student.unsw.edu.au>
2022-03-26 15:34:37 +1100

    feature: signup route basics and cleanup

--- src/server.py
+++ src/server.py
@@ -1,8 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+
+from src.auth import signup
+from src.database import db_cleanup
 
 APP = Flask(__name__)
 
 
 @APP.route("/")
-def default():
+def default_route():
     return "Hello, World!"
+
+
+@APP.route("/signup", methods=["POST"])
+def signup_route():
+    data = request.get_json()
+    res = signup(data)
+    print(res)
+    return ""
+
+
+@APP.route("/cleanup", methods=["POST"])
+def cleanup_route():
+    # DEV ONLY ROUTE SHOULD
+    # FIND A WAY TO REMOVE IN PROD
+    db_cleanup()
+    res = {"msg": "Removed all data stored in database."}
+    return res
