48d280929bfaac6b1c943fc9472ff4772b4085e5
Ethan Haffenden <z5367576@ad.unsw.edu.au>
2022-04-03 02:51:08 +1100

    Trying new solution

--- tests/test_send.py
+++ tests/test_send.py
@@ -8,20 +8,22 @@ from api_adapter.send import send_invoice
 VALID = 200
 INVALID_INPUT = 405
 
-@pytest.fixture
-def valid_invoice():
-    with open("tests/test_data/test_valid_response.xml") as sample_invoice_file:
-        return sample_invoice_file
+# @pytest.fixture
+# def valid_invoice():
+#     with open("tests/test_data/test_valid_response.xml") as sample_invoice_file:
+#         return sample_invoice_file
 
 
 def test_successful_ubl(valid_invoice):
+
+
 
     input = {
         "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDdkNWYxN2I0NDViNDlkOGI2MTZjMCIsImVtYWlsIjoiZXRoYW5oYWZmZW5kZW5AZ21haWwuY29tIiwiaWF0IjoxNjQ4ODc1MTc1fQ.LYXRVSvge2TUU6rv_4y2RWLgiaHDIhW1Ttd-ZRSHlm4",
         "invoiceTitle": "UBL!!!",
         "mailContent": "Here is your UBL",
         "recipientEmail": "z5367576@ad.unsw.edu.au",
-        "file": valid_invoice
+        "file": open("tests/test_data/test_valid_response.xml")
     }
 
     result = send_invoice(input)
