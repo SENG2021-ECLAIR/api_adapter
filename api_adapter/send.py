cc9c5b7b8ecaf351bee2b6140a334e5893de50b9
amazhangwinz <59673561+amazhangwinz@users.noreply.github.com>
2022-04-03 14:48:21 +1000

    Fixed send function

--- api_adapter/send.py
+++ api_adapter/send.py
@@ -5,27 +5,18 @@ Functionality for the send endpoint
 '''
 
 import requests
+ 
 
-    
-
-def send_invoice():
+def send_invoice(input):
     data1 = {
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDkxMzgyMzYxM2UzYTBjNjU2ZmQ4NiIsImVtYWlsIjoic2VuZzIwMjFlY2xhaXJAZ21haWwuY29tIiwiaWF0IjoxNjQ4OTU2NTQxfQ.MMog2AH6wNo7RRW5M3oy_0WGA4Kl5oj7rv0p6CrpXVw",
-        "invoiceTitle": "UBL!!!",
-        "mailContent": "Here is your UBL",
-        "recipientEmail": "z5367576@ad.unsw.edu.au"
+        "token": input["token"],
+        "invoiceTitle": input["invoiceTitle"],
+        "mailContent": input["mailContent"],
+        "recipientEmail": input["recipientEmail"]
     }
     data2 = {
-        "file": open("tests/test_data/test_valid_response.xml")
+        "file": input["file"]
     }
     url = "https://honeycomb-prod.herokuapp.com/send"
     post_val = requests.post(url, data=data1, files=data2)
     return post_val
-
-# def send_invoice_sms(input):
-#     post_val = requests.post("https://virtserver.swaggerhub.com/SE2Y22G24/e-invoice-sending/1.0.0/invoice/send/email",
-#         json = input)
-#     return {
-#         "status_code": post_val.status_code,
-#         "report_id": post_val.json()["report_id"]
-#     }
