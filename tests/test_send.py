be054c113ff6bc6a0af2079dc5db6ab3192e5b6f
amazhangwinz <59673561+amazhangwinz@users.noreply.github.com>
2022-04-03 14:57:36 +1000

    fixed memory leak

--- tests/test_send.py
+++ tests/test_send.py
@@ -9,12 +9,14 @@ VALID = 200
 INVALID_INPUT = 405
 
 def test_successful_ubl():
+    input_file = open("tests/test_data/test_valid_response.xml")
     input = {
         "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDkxMzgyMzYxM2UzYTBjNjU2ZmQ4NiIsImVtYWlsIjoic2VuZzIwMjFlY2xhaXJAZ21haWwuY29tIiwiaWF0IjoxNjQ4OTU2NTQxfQ.MMog2AH6wNo7RRW5M3oy_0WGA4Kl5oj7rv0p6CrpXVw",
         "invoiceTitle": "UBL!!!",
         "mailContent": "Here is your UBL",
         "recipientEmail": "z5367576@ad.unsw.edu.au",
-        "file": open("tests/test_data/test_valid_response.xml")
+        "file": input_file
     }
     result = send_invoice(input)
+    input_file.close()
     assert result.status_code == VALID
