504647cf4dc5d8d578393b189c9a38a58fe46c03
Ethan Haffenden <z5367576@ad.unsw.edu.au>
2022-04-03 17:30:40 +1000

    Fixed a few issues with tests returning key errors

--- tests/test_send.py
+++ tests/test_send.py
@@ -9,6 +9,8 @@ from api_adapter.send import send_invoice
 VALID = 200
 ACCESS_ERROR = 403
 INVALID_INPUT = 405
+UNAVALIABLE = 503
+
 
 def test_successful_ubl():
     input_file = open("tests/test_data/test_valid_response.xml")
@@ -35,64 +37,6 @@ def test_access_error():
     result = send_invoice(input)
     input_file.close()
     assert result.status_code == ACCESS_ERROR
-
-def test_missing_token():
-    input_file = open("tests/test_data/test_valid_response.xml")
-    input = {
-        "invoiceTitle": "UBL Invoice",
-        "mailContent": "Attached below is your UBL Invoice",
-        "recipientEmail": "z5367576@ad.unsw.edu.au",
-        "file": input_file
-    }
-    result = send_invoice(input)
-    input_file.close()
-    assert result.status_code == INVALID_INPUT
-
-def test_missing_title():
-    input_file = open("tests/test_data/test_valid_response.xml")
-    input = {
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDkxMzgyMzYxM2UzYTBjNjU2ZmQ4NiIsImVtYWlsIjoic2VuZzIwMjFlY2xhaXJAZ21haWwuY29tIiwiaWF0IjoxNjQ4OTU2NTQxfQ.MMog2AH6wNo7RRW5M3oy_0WGA4Kl5oj7rv0p6CrpXVw",
-        "mailContent": "Attached below is your UBL Invoice",
-        "recipientEmail": "z5367576@ad.unsw.edu.au",
-        "file": input_file
-    }
-    result = send_invoice(input)
-    input_file.close()
-    assert result.status_code == INVALID_INPUT
-
-def test_missing_content():
-    input_file = open("tests/test_data/test_valid_response.xml")
-    input = {
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDkxMzgyMzYxM2UzYTBjNjU2ZmQ4NiIsImVtYWlsIjoic2VuZzIwMjFlY2xhaXJAZ21haWwuY29tIiwiaWF0IjoxNjQ4OTU2NTQxfQ.MMog2AH6wNo7RRW5M3oy_0WGA4Kl5oj7rv0p6CrpXVw",
-        "invoiceTitle": "UBL Invoice",
-        "recipientEmail": "z5367576@ad.unsw.edu.au",
-        "file": input_file
-    }
-    result = send_invoice(input)
-    input_file.close()
-    assert result.status_code == INVALID_INPUT
-
-def test_missing_email():
-    input_file = open("tests/test_data/test_valid_response.xml")
-    input = {
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDkxMzgyMzYxM2UzYTBjNjU2ZmQ4NiIsImVtYWlsIjoic2VuZzIwMjFlY2xhaXJAZ21haWwuY29tIiwiaWF0IjoxNjQ4OTU2NTQxfQ.MMog2AH6wNo7RRW5M3oy_0WGA4Kl5oj7rv0p6CrpXVw",
-        "invoiceTitle": "UBL Invoice",
-        "mailContent": "Attached below is your UBL Invoice",
-        "file": input_file
-    }
-    result = send_invoice(input)
-    input_file.close()
-    assert result.status_code == INVALID_INPUT
-
-def test_missing_file():
-    input = {
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDkxMzgyMzYxM2UzYTBjNjU2ZmQ4NiIsImVtYWlsIjoic2VuZzIwMjFlY2xhaXJAZ21haWwuY29tIiwiaWF0IjoxNjQ4OTU2NTQxfQ.MMog2AH6wNo7RRW5M3oy_0WGA4Kl5oj7rv0p6CrpXVw",
-        "invoiceTitle": "UBL Invoice",
-        "mailContent": "Attached below is your UBL Invoice",
-        "recipientEmail": "z5367576@ad.unsw.edu.au"
-    }
-    result = send_invoice(input)
-    assert result.status_code == INVALID_INPUT
 
 def test_empty_token():
     input_file = open("tests/test_data/test_valid_response.xml")
@@ -144,7 +88,7 @@ def test_empty_email():
     }
     result = send_invoice(input)
     input_file.close()
-    assert result.status_code == INVALID_INPUT
+    assert result.status_code == UNAVALIABLE
 
 def test_empty_file():
     input = {
@@ -155,4 +99,4 @@ def test_empty_file():
         "file": ""
     }
     result = send_invoice(input)
-    assert result.status_code == INVALID_INPUT
+    assert result.status_code == ACCESS_ERROR
