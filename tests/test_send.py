f3eceb7a45fd086764b3844d1c7d02167ff83200
Ethan Haffenden <z5367576@ad.unsw.edu.au>
2022-04-03 13:29:59 +1000

    Trying new solution

--- tests/test_send.py
+++ tests/test_send.py
@@ -19,7 +19,7 @@ def test_successful_ubl():
 
 
     input = {
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDdkNWYxN2I0NDViNDlkOGI2MTZjMCIsImVtYWlsIjoiZXRoYW5oYWZmZW5kZW5AZ21haWwuY29tIiwiaWF0IjoxNjQ4ODc1MTc1fQ.LYXRVSvge2TUU6rv_4y2RWLgiaHDIhW1Ttd-ZRSHlm4",
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDkxMzgyMzYxM2UzYTBjNjU2ZmQ4NiIsImVtYWlsIjoic2VuZzIwMjFlY2xhaXJAZ21haWwuY29tIiwiaWF0IjoxNjQ4OTU2NTQxfQ.MMog2AH6wNo7RRW5M3oy_0WGA4Kl5oj7rv0p6CrpXVw",
         "invoiceTitle": "UBL!!!",
         "mailContent": "Here is your UBL",
         "recipientEmail": "z5367576@ad.unsw.edu.au",
