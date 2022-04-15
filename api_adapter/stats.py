05c055ef78317b0d1d9406ffac65d40f1e7a2db6
Winnie Zhang <winnie.zhang825@gmail.com>
2022-04-16 01:40:06 +1000

    :sparkles: Keep track of how many invoices were created

---
+++ api_adapter/stats.py
@@ -0,0 +1,20 @@
+"""
+Calculate and create the stats of the users.
+"""
+
+from api_adapter.database import get_invoices
+import datetime, xmltodict
+
+def num_created_stats(token):
+    """
+    How many invoices created is returned.
+    """
+
+    invoices, msg = get_invoices(token)
+
+    created_invoices = invoices["created"]
+
+    return {
+        "msg": msg,
+        "num_created_inv": len(invoices["created"])
+    }
