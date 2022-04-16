a460595c6c2b0c079565cb5bb0c46594a3979a71
RohanA <rohanagarwal0192@gmail.com>
2022-04-16 11:19:04 +1000

    change send_invoice return value

--- api_adapter/send.py
+++ api_adapter/send.py
@@ -4,7 +4,9 @@ Functionality for the send endpoint
 - Generates a status report
 """
 
-import requests, os
+import os
+
+import requests
 
 
 def send_invoice(input):
@@ -24,4 +26,4 @@ def send_invoice(input):
     post_val = requests.post(url, data=data1, files=data2)
     file.close()
     os.remove("xml_file.xml")
-    return post_val
+    return {"reponse": post_val}
