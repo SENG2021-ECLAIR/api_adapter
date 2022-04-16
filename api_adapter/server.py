c859a9d5bda804d6a78f436817425b56052b13ab
Winnie Zhang <winnie.zhang825@gmail.com>
2022-04-16 15:08:44 +1000

    :sparkles: make sure the months return both month and past

--- api_adapter/server.py
+++ api_adapter/server.py
@@ -30,6 +30,8 @@ from api_adapter.render import get_render
 from api_adapter.send import send_invoice
 from api_adapter.team import create_team, invite_member, list_team_members
 from api_adapter.users import list_users
+from api_adapter.stats import curr_month_stats
+
 
 APP = Flask(__name__)
 CORS(APP)
@@ -209,6 +211,12 @@ def team_members_route():
     logging.info(response)
     return response
 
+@APP.route("/stats/month", methods=["GET"])
+def daily_stats():
+    token = request.headers.get("token")
+    if token is None:
+        return {"msg": "Needs token in headers"}
+    return json.dumps(curr_month_stats(token))
 
 @APP.route("/test", methods=["POST"])
 def test_route():
--- api_adapter/stats.py
+++ api_adapter/stats.py
@@ -20,18 +20,84 @@ def curr_month_stats(token):
     today_date = datetime.datetime.now()
 
     for invoice in created_invoices:
-        time_of_invoice = invoice["invoices"]["timestamp"]
+        time_of_invoice = invoice["timestamp"]
 
         # parse datetime
         invoice_datetime = datetime.datetime.strptime(
             time_of_invoice, "%d/%m/%Y, %H:%M:%S"
         )
-        if invoice_datetime.Month == today_date.Month:
-            inv_dict = xmltodict.parse(invoice["invoices"]["content"])
-            monetary = inv_dict["Invoice"]["cac:LegalMonetaryTotal"]
-            month_earns += float(monetary["cbc:PayableAmount"]["#text"])
+        if (invoice_datetime.month == today_date.month
+            and invoice_datetime.year == today_date.year):
+            # inv_dict = xmltodict.parse(invoice["invoices"]["content"])
+            # monetary = inv_dict["Invoice"]["cac:LegalMonetaryTotal"]
+            # month_earns += float(monetary["cbc:PayableAmount"]["#text"])
+            month_earns += 1
+    
+    for invoice in invoices["received"]:
+        time_of_invoice = invoice["timestamp"]
+
+        # parse datetime
+        invoice_datetime = datetime.datetime.strptime(
+            time_of_invoice, "%d/%m/%Y, %H:%M:%S"
+        )
+        if (invoice_datetime.month == today_date.month
+            and invoice_datetime.year == today_date.year):
+            # inv_dict = xmltodict.parse(invoice["invoices"]["content"])
+            # monetary = inv_dict["Invoice"]["cac:LegalMonetaryTotal"]
+            # month_earns += float(monetary["cbc:PayableAmount"]["#text"])
+            month_earns += 1
+
+    list_prev_months = []
+
+    for _ in range(0,5):
+        list_prev_months.append(0)
+
+    # find the date 5 months ago
+    start_date = today_date - datetime.timedelta(5*30)
+
+    curr_date = today_date
+
+    i = 0
+
+
+    while (curr_date != start_date):
+
+        for invoice in invoices["created"]:
+            time_of_invoice = invoice["timestamp"]
+
+            # parse datetime
+            invoice_datetime = datetime.datetime.strptime(
+                time_of_invoice, "%d/%m/%Y, %H:%M:%S"
+            )
+
+            if (invoice_datetime.month == curr_date.month
+                and invoice_datetime.year == curr_date.year):
+                # inv_dict = xmltodict.parse(invoice["invoices"]["content"])
+                # monetary = inv_dict["Invoice"]["cac:LegalMonetaryTotal"]
+                # list_stats[i] += float(monetary["cbc:PayableAmount"]["#text"])
+                list_prev_months[i] += 1
+
+        for invoice in invoices["received"]:
+            time_of_invoice = invoice["timestamp"]
+
+            # parse datetime
+            invoice_datetime = datetime.datetime.strptime(
+                time_of_invoice, "%d/%m/%Y, %H:%M:%S"
+            )
+
+            if (invoice_datetime.month == curr_date.month
+                and invoice_datetime.year == curr_date.year):
+                # inv_dict = xmltodict.parse(invoice["invoices"]["content"])
+                # monetary = inv_dict["Invoice"]["cac:LegalMonetaryTotal"]
+                # list_stats[i] += float(monetary["cbc:PayableAmount"]["#text"])
+                list_prev_months[i] += 1
+
+        # loop back each date
+        curr_date -= datetime.timedelta(30)
+        i += 1
 
     return {
         "msg": msg,
-        "month_earns": month_earns
+        "month_earns": month_earns,
+        "last_five_months": list_prev_months
     }
